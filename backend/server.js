f92c8e914798ecfd8ceeab38edac33f436aad43f
Rakesh Biswal <rb2306114@gmail.com>
2025-09-21 21:43:06 +0530

    cfgv

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-require("dotenv").config({ path: __dirname + "/../.env" });
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const connectDB = require("./config/db");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db'); 
 const jobApplicationRoutes = require("./routes/ClientRoutes/JobApplicationRoute");
 
 // Imported All Routes
