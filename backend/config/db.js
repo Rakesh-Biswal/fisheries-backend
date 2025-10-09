e47a3f705750e8f9a7ad9b3b261b52b4c3ca9b2b
sonu4435 <soumyaranjanpraharaj04@gmail.com>
2025-10-09 13:56:50 +0530

    db.js chnged

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,22 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // use env first, fallback if undefined
+    const uri =
+      process.env.MONGO_URI ||
+      "mongodb+srv://pramod18:pramod85%40biswal@cluster0.cny7fgt.mongodb.net/fisheries_platform";
+
+    console.log("📡 Using Mongo URI:", uri);
+
+    const conn = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Failed: ${error.message}`);
-    process.exit(1); // Stop the app if DB fails
+    process.exit(1);
   }
 };
 
